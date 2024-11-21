2b34d74142ca05893df6ca45d80362ad2eb8bb6d
IsaiGr <galiciaramirezisai@gmail.com>
2024-11-20 19:28:39 -0600

    Add files via upload

---
+++ ENUMERATE.py
@@ -0,0 +1,10 @@
+lista = ['a','b','c','d','e']
+indice = 0
+for item,indice in enumerate(lista):
+    print( item, indice)
+
+
+#EJEMPLO CON TUPLAS Y SIN LOOP
+pract = ['x','y','x']
+mis_tuplas = list(enumerate(pract))
+print(mis_tuplas)    
